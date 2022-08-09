9703b9eda41502620f44e9337aea7aa360637521
yangabrp <yg@forpeople.io>
2022-08-09 14:18:22 -0300

    implementação dos outros buttons

--- src/components/pomodoroTimer.tsx
+++ src/components/pomodoroTimer.tsx
@@ -3,6 +3,7 @@ import { useInterval } from "../hooks/hooks";
 import { secondsToTime } from "../utils/secondsToTime";
 import { Button } from "../components/Button/Button";
 import { Timer } from "../components/Timer/Timer";
+import { Pomodoro } from "../styles/GlobalStyles";
 interface Props {
   pomodoroTime: number;
   shortRestTime: number;
@@ -11,12 +12,17 @@ interface Props {
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
   useInterval(() => {
-    setMainTime(mainTime + 1);
+    setMainTime(mainTime - 1);
   }, 1000);
   return (
     <div>
-      <Button text="Play timer"></Button>
-      <Timer mainTime={mainTime} />
+      <Pomodoro>
+        <h2>Working</h2>
+        <Timer mainTime={mainTime} />
+        <Button text="Play timer"></Button>
+        <Button text=""></Button>
+        <Button text=""></Button>
+      </Pomodoro>
     </div>
   );
 }
